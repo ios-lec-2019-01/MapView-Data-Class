62087be7c1cd0b0a8351df1fa7a66100acd7bf17
D7702_09 <d7702_09@d7702-09ui-imac.local>
2019-09-17 13:17:13 +0900

    Initial Commit

---
+++ SimpleMapView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleMapView
+//
+//  Created by D7702_09 on 2019. 9. 17..
+//  Copyright © 2019년 csd5766. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
